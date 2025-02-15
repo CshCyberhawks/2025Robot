0bc6ac4dcc0a148ed32c2a6fae23ee0a4c785319
Ryan Smith <62762243+PY44N@users.noreply.github.com>
2025-02-14 19:16:03 -0700

    Wrist doesn't exist anymore

--- src/main/java/frc/robot/subsystems/superstructure/Superstructure.kt
+++ src/main/java/frc/robot/subsystems/superstructure/Superstructure.kt
@@ -7,12 +7,9 @@ import frc.robot.subsystems.superstructure.intake.implementation.IntakeIOEmpty
 import frc.robot.subsystems.superstructure.pivot.PivotSystem
 import frc.robot.subsystems.superstructure.pivot.implementation.PivotIOEmpty
 import frc.robot.subsystems.superstructure.intake.IntakeSystem
-import frc.robot.subsystems.superstructure.wrist.WristSystem
-import frc.robot.subsystems.superstructure.wrist.implementation.WristIOEmpty
 
 class Superstructure() : SubsystemBase() {
     val pivotSystem = PivotSystem(PivotIOEmpty())
-    val wristSystem = WristSystem(WristIOEmpty())
     val elevatorSystem = ElevatorSystem(ElevatorIOEmpty())
     val intakeSystem = IntakeSystem(IntakeIOEmpty())
 }
