3b503d2dc234026db2028c8f807705925a21f5fd
Ryan Smith <62762243+PY44N@users.noreply.github.com>
2025-02-28 10:05:37 -0500

    Swapped it around so we are implementing in the real robot

--- src/main/java/frc/robot/RobotConfiguration.kt
+++ src/main/java/frc/robot/RobotConfiguration.kt
@@ -12,7 +12,8 @@ enum class OperatorType {
 }
 
 object RobotConfiguration {
-    val robotType = RobotType.Empty
+    val robotType = RobotType.Real
 
     val operatorType = OperatorType.Manual
 }
+
